ee7efb931950d2607117ae5a7617ef0cfd9bd10d
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-03-08 21:30:18 +0300

    update js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './style.css';
+import { addScore, getData } from './modules/addScore.js';
+import renderList from './modules/renderList.js';
+
+const refreshBtn = document.getElementById('refresh-btn');
+const submitBtn = document.getElementById('submit-btn');
+const userInput = document.getElementById('user-input');
+const scoreInput = document.getElementById('score-input');
+
+const upload = async (e) => {
+  e.preventDefault();
+  await addScore(userInput.value, scoreInput.value);
+  userInput.value = '';
+  scoreInput.value = '';
+
+  const display = await getData();
+  renderList(display.result);
+};
+
+submitBtn.addEventListener('click', upload);
+
+const refresh = async () => {
+  const get = await getData();
+  renderList(get.result);
+};
+
+refreshBtn.addEventListener('click', () => refresh);
+
+window.onload = refresh;
